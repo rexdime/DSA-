55bc073284470520e10143276c932180fac136f2
rexdime <sirohidaksh80@gmail.com>
2026-09-04 20:27:38 +0530

    Time: 4 ms (99.52%), Space: 159.8 MB (64.68%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,60 +1,24 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
-
-        for (int i : nums) {
-            if (i < pivot) {
-                count1++;
-            } else if (i == pivot) {
-                count2++;
-            } else {
-                count3++;
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+        for(int i=left;i<nums.length;i++){
+            if(nums[i]<pivot){
+                result[left]=nums[i];
+                left++;
             }
         }
-
-        int left[] = new int[count1];
-        int mid[] = new int[count2];
-        int right[] = new int[count3];
-
-        int ind1 = 0;
-        int ind2 = 0;
-        int ind3 = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < pivot) {
-                left[ind1] = nums[i];
-                ind1++; 
-            } else if (nums[i] == pivot) {
-                mid[ind2] = nums[i];
-                ind2++;
-            } else {
-                right[ind3] = nums[i];
-                ind3++;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>pivot){
+                result[right]=nums[i];
+                right--;
             }
         }
-
-        
-        int current = 0;
-        int result[] = new int[nums.length];
-
-        
-        for (int i = 0; i < left.length; i++) {
-            result[current] = left[i];
-            current++;
+        for(int i=left;i<=right;i++) {
+            result[i]=pivot;
         }
-        for (int i = 0; i < mid.length; i++) {
-            result[current] = mid[i];
-            current++;
-        }
-
-    
-        for (int i = 0; i < right.length; i++) {
-            result[current] = right[i];
-            current++;
-        }
-
         return result;
     }
 }
